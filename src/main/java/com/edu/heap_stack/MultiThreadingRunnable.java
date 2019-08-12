00d4b2c203af044d607fd741347de5814f81efa5
sontruong <truongdovinhson@gmail.com>
2019-08-13 00:31:44 +0700

    update code

---
+++ src/main/java/com/edu/heap_stack/MultiThreadingRunnable.java
@@ -0,0 +1,37 @@
+package com.edu.heap_stack;
+
+public class MultiThreadingRunnable implements Runnable {
+	public static void main(String[] args) {
+		createSimpleThread();
+		createSimpleWithPriority();
+	}
+
+	private static void createSimpleWithPriority() {
+		Thread guruthread1 = new Thread();
+        guruthread1.start();
+        try {
+            guruthread1.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        guruthread1.setPriority(1);
+        int gurupriority = guruthread1.getPriority();
+        System.out.println("gurupriority: " + gurupriority);
+        System.out.println("Thread Running");
+	}
+
+	private static void createSimpleThread() {
+		Thread guruThread1 = new Thread("Guru1");
+        Thread guruThread2 = new Thread("Guru2");
+        guruThread1.start();
+        guruThread2.start();
+        System.out.println("Thread names are following:");
+        System.out.println(guruThread1.getName());
+        System.out.println(guruThread2.getName());
+	}
+
+	public void run() {
+		
+	}
+}
