8a3d6efd8c3246ef6fa3b4cdac34fb23777c6053
NGOCLY-LAPTOP\SonTruong <truongdovinhson@gmail.com>
2019-08-18 10:01:59 +0700

    update code

--- src/main/java/com/edu/text/TextBreakIterator.java
+++ src/main/java/com/edu/text/TextBreakIterator.java
@@ -1,9 +1,22 @@
 package com.edu.text;
 
+import java.text.BreakIterator;
+import java.util.Locale;
+
+import com.edu.utils.FileUtils;
+
 public class TextBreakIterator {
 	
 	public static void main(String[] strs) {
-		
+		BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+		String str = FileUtils.readLineByLineJava8("D:\\AppData\\bigdata\\java-breakiterator.txt");
+		iterator.setText(str);
+		int start = iterator.first();
+		for (int end = iterator.next();
+		    end != BreakIterator.DONE;
+		    start = end, end = iterator.next()) {
+			System.out.println(str.substring(start,end));
+		}
 	}
 	
 	
